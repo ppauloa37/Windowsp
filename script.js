e5e7ed1138742e5aa85250c0c201aa9e5314a2be
ppauloa37 <ppauloa37@gmail.com>
2025-01-14 15:05:39 -0300

    script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+function setup() {
+  let canvas = createCanvas(800, 600);
+  canvas.parent('canvas');
+  background(240);
+}
+
+function draw() {
+  // Lógica do sistema operacional
+  fill(0);
+  textSize(16);
+  text('Bem-vindo ao Sistema Operacional Simulado!', 50, 50);
+}
