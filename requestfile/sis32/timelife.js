77879c0f5dc4774446987c94a437eccd0ea2d287
Paulfox <ppauloa37@gmail.com>
2025-05-19 10:32:59 -0300

    Create timelife.js

---
+++ requestfile/sis32/timelife.js
@@ -0,0 +1,13 @@
+var clock = document.getElementById("clock");
+var clock2 = document.getElementById("barclock");
+
+var hourUpdate = function() {
+  var data = new Date()
+  var Hora = data.getHours()
+  var Minuto = data.getMinutes()
+  var Segundo = data.getSeconds()
+  clock.textContent = Hora + ":" + Minuto;
+  clock2.textContent = Hora + ":" + Minuto;
+});
+
+window.setInterval(hourUpdate,1000);
